4539ff3ede5410e8236ff2260b7780e84f54f95d
hengchu <hengchu.zhang@gmail.com>
2019-08-11 22:08:15 -0400

    adding distribution provenance types

--- src/Distribution.hs
+++ src/Distribution.hs
@@ -7,6 +7,7 @@ import Data.Random.Distribution.Uniform
 import Data.Random.Distribution.Normal
 import Control.Monad.IO.Class
 import Types
+import Type.Reflection
 
 newtype ConcreteDist a = ConcreteDist { runConcreteDist :: RVar a }
   deriving (Functor, Applicative, Monad)
@@ -31,6 +32,9 @@ sampleConcrete = R.sample . runConcreteDist
 data ArithOp = Add | Mult | Sub | Div
   deriving (Show, Eq, Ord)
 
+data UOp = Abs | Sign
+  deriving (Show, Eq, Ord)
+
 data DistributionProvenance (a :: *) where
   Deterministic :: a -> DistributionProvenance a
   Laplace       :: a -> Double -> DistributionProvenance a
@@ -39,7 +43,52 @@ data DistributionProvenance (a :: *) where
                 -> ArithOp
                 -> DistributionProvenance a
                 -> DistributionProvenance a
-  deriving (Show, Eq, Ord)
+  Unary         :: UOp
+                -> DistributionProvenance a
+                -> DistributionProvenance a
+  deriving (Show, Eq, Ord, Functor, Typeable)
+
+instance Num a => Num (DistributionProvenance a) where
+  a + b         = Arith a Add  b
+  a * b         = Arith a Mult b
+  a - b         = Arith a Sub  b
+  abs a         = Unary Abs  a
+  signum a      = Unary Sign a
+  fromInteger v = Deterministic (fromInteger v)
+
+instance Fractional a => Fractional (DistributionProvenance a) where
+  a / b          = Arith a Div b
+  fromRational v = Deterministic (fromRational v)
+
+data WithDistributionProvenance a =
+  WithDistributionProvenance { value :: a
+                             , provenance :: DistributionProvenance a
+                             }
+  deriving (Show, Eq, Ord, Functor, Typeable)
+
+instance Num a => Num (WithDistributionProvenance a) where
+  a + b = WithDistributionProvenance (value a + value b) (provenance a + provenance b)
+  a * b = WithDistributionProvenance (value a * value b) (provenance a * provenance b)
+  a - b = WithDistributionProvenance (value a - value b) (provenance a - provenance b)
+  abs a = WithDistributionProvenance (abs (value a)) (abs (provenance a))
+  signum a = WithDistributionProvenance (signum (value a)) (signum (provenance a))
+  fromInteger v = WithDistributionProvenance (fromInteger v) (fromInteger v)
+
+instance Fractional a => Fractional (WithDistributionProvenance a) where
+  a / b = WithDistributionProvenance (value a / value b) (provenance a / provenance b)
+  fromRational v = WithDistributionProvenance (fromRational v) (fromRational v)
+
+instance Ordered a => Ordered (WithDistributionProvenance a) where
+  type CmpResult (WithDistributionProvenance a) = CmpResult a
+  a %<  b = value a %<  value b
+  a %<= b = value a %<= value b
+  a %>  b = value a %>  value b
+  a %>= b = value a %>= value b
+  a %== b = value a %== value b
+  a %/= b = value a %/= value b
+
+instance Numeric a     => Numeric     (WithDistributionProvenance a)
+instance FracNumeric a => FracNumeric (WithDistributionProvenance a)
 
 instance MonadDist ConcreteDist where
   type NumDomain ConcreteDist = Double
